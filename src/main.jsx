845b246e9cd19d7efb848c42926806ae6f2c78b1
Paulina Kiendys <kiendyspaulina@gmail.com>
2024-02-29 12:06:40 +0100

    add router, root route, handle Not Found errors and nested dashboard route

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+
+import Root from "./routes/Root";
+import NotFound from "./routes/NotFound";
+import Dashboard from "./routes/Dashboard";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Navigate replace to="dashboard" />,
+      },
+      { path: "dashboard", element: <Dashboard /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
