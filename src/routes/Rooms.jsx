f8188459b55bf645286e29fe0abf431b660c9de3
Paulina Kiendys <kiendyspaulina@gmail.com>
2024-03-01 12:20:05 +0100

    display rooms in a table, add alert and spinner component

--- src/routes/Rooms.jsx
+++ src/routes/Rooms.jsx
@@ -1,44 +1,72 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import { getRooms } from "../services/apiRooms";
+import Spinner from "../components/Spinner";
+import Alert from "../components/Alert";
 
 export default function Rooms() {
-  const [rooms, setRooms] = useState([]);
-  useEffect(() => {
-    async function fetchRooms() {
-      try {
-        const fetchedRooms = await getRooms();
-        setRooms(fetchedRooms || []);
-      } catch (error) {
-        console.error("Error fetching rooms:", error.message);
-      }
-    }
+  const {
+    isLoading,
+    data: rooms = [],
+    error,
+  } = useQuery({
+    queryKey: ["rooms"],
+    queryFn: getRooms,
+  });
 
-    fetchRooms();
-  }, []);
+  const tableHeaders = ["", "Room", "Capacity", "Rate", "Discount", ""];
+
+  if (isLoading) return <Spinner />;
+  if (error) return <Alert variant="danger" message={error.message} />;
 
   return (
     <>
       <h2>Rooms</h2>
-      {rooms.length === 0 ? (
-        <p>No rooms available.</p>
+      {!isLoading && rooms.length === 0 ? (
+        <Alert variant="info" message="No rooms available" />
       ) : (
-        <ul>
-          {rooms.map((room) => (
-            <li key={room.number}>
-              <h3>{room.number}</h3>
-              <p>Capacity: {room.capacity}</p>
-              <p>Rate: ${room.rate}</p>
-              <p>Discount: {room.discount * 100}%</p>
-              <p>Description: {room.description}</p>
-              <img
-                src={room.imageUrl}
-                alt={`Room ${room.number}`}
-                style={{ maxWidth: "200px" }}
-              />
-            </li>
-          ))}
-        </ul>
+        <div className="table-responsive">
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                {tableHeaders.map((header, index) => (
+                  <th key={index} scope="col" className="text-center">
+                    {header}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {rooms.map(
+                ({ id, imageUrl, number, capacity, rate, discount }) => (
+                  <tr key={id}>
+                    <th scope="row" className="text-center">
+                      <img
+                        src={imageUrl}
+                        alt={`Room ${number}`}
+                        style={{ maxWidth: "100px", maxHeight: "100px" }}
+                      />
+                    </th>
+                    <td className="text-center align-middle">{number}</td>
+                    <td className="text-center align-middle">{capacity}</td>
+                    <td className="text-center align-middle">${rate}</td>
+                    <td className="text-center align-middle">
+                      {discount * 100}%
+                    </td>
+                    <td className="text-center align-middle">
+                      <div className="btn-group" role="group">
+                        <button className="btn btn-primary">Duplicate</button>
+                        <button className="btn btn-warning">Edit</button>
+                        <button className="btn btn-danger">Delete</button>
+                      </div>
+                    </td>
+                  </tr>
+                )
+              )}
+            </tbody>
+          </table>
+        </div>
       )}
+      <button className="btn btn-success">Add room</button>
     </>
   );
 }
